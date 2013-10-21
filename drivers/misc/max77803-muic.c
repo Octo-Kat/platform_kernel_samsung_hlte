fe249e1e6d5512459f7512b7a271c5138dcc36d2
Paul Reioux <reioux@gmail.com>
2013-10-21 00:12:05 -0500

    compilation: fix up for mismatched type for system_rev
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/misc/max77803-muic.c
+++ drivers/misc/max77803-muic.c
@@ -960,7 +960,7 @@ void max77803_muic_usbdelay_detach_work(struct max77803_muic_info *info)
 }
 #endif
 
-extern int system_rev;
+extern unsigned int system_rev;
 static int max77803_muic_set_charging_type(struct max77803_muic_info *info,
 					   bool force_disable)
 {
